2b09dbbdf190d0f23ca45cdf7306b8288a9e193b
Nebojsa Pavlovic <nebojsap82@gmail.com>
2023-06-05 11:41:27 +0200

    cookieConsent set

--- js.js
+++ js.js
@@ -91,6 +91,36 @@ const model = {
       return '';
     }
   };  
+
+  // Check if the user has already given consent
+if (!localStorage.getItem('cookieConsent')) {
+  // Display a consent banner or popup to the user
+  // You can customize this part according to your needs
+  const consentBanner = document.createElement('div');
+  consentBanner.innerHTML = `
+    <div id="cookieConsentBanner" style="display: flex; justify-content: center; align-items: center; position: fixed; bottom: 0; left: 0; width: 100%; background-color: rgba(0, 0, 0, 0.199); padding: 3%;">
+      <p>This website uses cookies to improve your experience. By continuing to use this site, you consent to the use of cookies.</p>
+      <button id="acceptCookiesButton" style="margin-left: 1%; padding: 0.4%;">Accept</button>
+    </div>
+  `;
+
+  // Append the consent banner to the document body
+  document.body.appendChild(consentBanner);
+
+  // Handle the accept button click event
+  const acceptCookiesButton = document.getElementById('acceptCookiesButton');
+  acceptCookiesButton.addEventListener('click', () => {
+    // Set the cookie consent in local storage
+    localStorage.setItem('cookieConsent', true);
+    // Remove the consent banner
+    document.getElementById('cookieConsentBanner').remove();
+    // Place your first-party cookie here
+    document.cookie = "myCookie=example; expires=Thu, 01 Jan 2024 00:00:00 UTC; path=/;";
+  });
+} else {
+  // User has already given consent, place your first-party cookie directly
+  document.cookie = "myCookie=example; expires=Thu, 01 Jan 2024 00:00:00 UTC; path=/;";
+}
   
   controller.init();
   

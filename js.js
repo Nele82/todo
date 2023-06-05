00880fcbbe0684fe10d8f6ee53535ee02da3a734
Nebojsa Pavlovic <nebojsap82@gmail.com>
2023-06-05 08:18:51 +0200

    setCookie method adjusted

--- js.js
+++ js.js
@@ -74,7 +74,7 @@ const model = {
       const date = new Date();
       date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
       const expires = 'expires=' + date.toUTCString();
-      document.cookie = name + '=' + value + ';' + expires + ';';
+      document.cookie = name + '=' + value + ';' + expires + '; path=/;';
     },
   
     getCookie: function(name) {
